020582345649234809cfea06e821890c542cee69
ren89 <ren.avellano@gmail.com>
2024-10-07 18:56:51 +0800

    change colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="bg-[#000435]">
+      <body className="bg-slate-900">
         <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
           {children}
         </div>
--- components/main/ProjectSection.tsx
+++ components/main/ProjectSection.tsx
@@ -10,7 +10,7 @@ export function ProjectSection({ projects }: { projects: Project[] }) {
         {projects.map((project) => (
           <li className="mb-12" key={project.name}>
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-[#03123d] lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-[#6c7a97] sm:col-span-2">
                 <Image
                   src={project.image}
