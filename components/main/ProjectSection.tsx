a5bcc70fcdbb6150542a04ed41f2acdc5361b708
ren89 <ren.avellano@gmail.com>
2024-10-07 18:10:06 +0800

    add key

--- components/main/ProjectSection.tsx
+++ components/main/ProjectSection.tsx
@@ -2,14 +2,13 @@ import { Project } from "@/lib/types";
 import { ChipGroup } from "./chipGroup";
 import { Section } from "./section";
 import Image from "next/image";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export function ProjectSection({ projects }: { projects: Project[] }) {
   return (
     <Section title="Projects">
       <ol className="group/list">
         {projects.map((project) => (
-          <li className="mb-12">
+          <li className="mb-12" key={project.name}>
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-[#03123d] lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-[#6c7a97] sm:col-span-2">
