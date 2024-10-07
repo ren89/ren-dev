93860cb84dd76518baf7f18022702ef4f4e5b4bd
ren89 <ren.avellano@gmail.com>
2024-10-07 18:50:07 +0800

    add time working in web

--- components/header/content.tsx
+++ components/header/content.tsx
@@ -1,4 +1,20 @@
+"use client";
+import { formatDateDifference } from "@/lib/utils";
+import { useEffect, useState, useMemo } from "react";
+
 export function Content() {
+  const pastDate = useMemo(() => new Date("2020-06-01T00:00:00Z"), []);
+  const [timeDifference, setTimeDifference] = useState(
+    formatDateDifference(pastDate)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeDifference(formatDateDifference(pastDate));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [pastDate]);
   return (
     <div>
       <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
@@ -7,10 +23,9 @@ export function Content() {
       <h2 className="mt-3 text-lg font-medium tracking-tight sm:text-xl">
         Full Stack Web Developer
       </h2>
-      {/*
-TODO: add short bio 
-  <p className="mt-4 max-w-xs leading-normal"> </p> 
-*/}
+      <p className="mt-4 max-w-xs leading-normal">
+        I have been working with web technologies for {timeDifference}
+      </p>
     </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import {
+  differenceInYears,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+} from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDateDifference(pastDate: Date): string {
+  const now = new Date();
+
+  const years = differenceInYears(now, pastDate);
+  const days = differenceInDays(now, pastDate) % 365;
+  const hours = differenceInHours(now, pastDate) % 24;
+  const minutes = differenceInMinutes(now, pastDate) % 60;
+  const seconds = differenceInSeconds(now, pastDate) % 60;
+
+  return `${years} years, ${days} days, ${hours} hours, ${minutes} mins, ${seconds} secs`;
 }
