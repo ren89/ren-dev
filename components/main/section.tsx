15efb25ca54567a6dabe63d1ffd447f2ce77c8c3
ren89 <ren.avellano@gmail.com>
2024-12-23 17:15:41 +0800

    fix: add motion to section title

--- components/main/section.tsx
+++ components/main/section.tsx
@@ -1,20 +1,28 @@
-import React from "react";
-interface SectionProps {
-  title: string;
+import { motion } from "framer-motion";
+
+export function Section({
+  children,
+  title,
+}: {
   children: React.ReactNode;
-}
-export const Section: React.FC<SectionProps> = ({ title, children }) => {
+  title?: string;
+}) {
   return (
     <section
       className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
       id={title?.toLowerCase()}
     >
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-800 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+        <motion.h2
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only"
+        >
           {title}
-        </h2>
+        </motion.h2>
       </div>
       {children}
     </section>
   );
-};
+}
