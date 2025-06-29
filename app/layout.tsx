c00e9249912f67f8c076b9b2e40d40d6c4e83d6a
ren89 <ren.avellano@gmail.com>
2025-06-29 10:57:54 +0800

    use layout for header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,32 @@
 import { FC, ReactNode } from "react";
 import "../styles/globals.css";
+import { getUser } from "@/hooks/useUser";
+import { logout } from "./auth/actions";
 
-const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
+const RootLayout: FC<{ children: ReactNode }> = async ({ children }) => {
+  const user = await getUser();
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-slate-900">
-        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <header className=" bg-teal-400/10  text-center">
+          {user && (
+            <form>
+              <button
+                className="cursor-pointer"
+                type="submit"
+                formAction={logout}
+              >
+                <p className="text-teal-300">
+                  logged in as <b>{user.email}</b>
+                </p>
+                <p className="text-teal-300">click to logout</p>
+              </button>
+            </form>
+          )}
+        </header>
+        <main className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
           {children}
-        </div>
+        </main>
       </body>
     </html>
   );
